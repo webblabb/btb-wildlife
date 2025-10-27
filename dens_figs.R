6d836c281b8e007a9d5ea3dd523cf4ab244afb14
Sam Smith <sm.smith@colostate.edu>
2025-10-27 13:36:51 -0600

    updated scripts to just generate figures

--- dens_figs.R
+++ dens_figs.R
@@ -1,8 +1,31 @@
-#------------------------------------------------------------
-# Frequency-dependent transmission figures
+###############################################################
+## Compile and run code ##
+###############################################################
+
+system("g++ -L/usr/lib/x86_64-linux-gnu bTB_wildlifeModel_CTMC.cpp -lgsl -lgslcblas -lm -o wl_model_CTMC.exe")
+system("g++ -L/usr/lib/x86_64-linux-gnu bTB_wildlifeModel_DTMC.cpp -lgsl -lgslcblas -lm -o wl_model_DTMC.exe")
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(deSolve)
+  library(ggplot2)
+  library(ggpubr)
+  library(foreach)
+  library(doParallel)
+  library(RColorBrewer)
+  library(scales)
+})
+
+source("bTBwl_func.R")
+
+dir.create("data",      showWarnings = FALSE, recursive = TRUE)
+dir.create("data/raw",  showWarnings = FALSE, recursive = TRUE)
+dir.create("figures/supp",   showWarnings = FALSE, recursive = TRUE)
+
 # ------------------------------------------------------------
-system("g++ -L/usr/lib/x86_64-linux-gnu tb_wildlife_freq_cont.cpp -lgsl -lgslcblas -lm -o wl_model_CTMC.exe")
-system("g++ -L/usr/lib/x86_64-linux-gnu tb_wildlife_freq_disc.cpp -lgsl -lgslcblas -lm -o wl_model_DTMC.exe")
+# Main text figures
+# ------------------------------------------------------------
+
 ## -----------------------------------------------------------
 ## Figure 1: size x infType
 
@@ -14,12 +37,13 @@ param_grid <- expand_grid(
 for (i in 1:nrow(param_grid)) {
   row <- param_grid[i,]
   
-  run_wl_sim(size = row$size, infType = row$infType, save_plots = TRUE, freq = T) 
+  run_wl_sim(size = row$size, infType = row$infType, save_plots = TRUE, ode = T) 
 }
 
 ## -----------------------------------------------------------
 ## Figure 2: super-spreader prop x herd size
-results <- read.csv("data/ss_herdsize_meanprev_freq.csv")
+
+results <- read.csv("data/ss_herdsize_meanprev.csv")
 
 mean_map <- ggplot(
   data = results,
@@ -31,8 +55,8 @@ mean_map <- ggplot(
 ) +
   geom_tile(color = NA) +  # remove borders between tiles
   scale_fill_distiller(palette = "Spectral",
-                       breaks = round(seq(0,max(results$mean_prev), length.out = 4),4),   # manually specify desired breaks
-                       limits = c(0, round(max(results$mean_prev),3))) +
+                       breaks = round(seq(0,max(results$mean_prev), length.out = 4),1),   # manually specify desired breaks
+                       limits = c(0, 1)) +
   scale_x_continuous(breaks = c(25, 250, 500, 750, 1000)) +
   labs(
     fill = str_wrap("mean infection prevalence", 0),
@@ -53,10 +77,10 @@ mean_map <- ggplot(
 
 mean_map
 
-ggsave("figures/supp/mean_prevalence_map_freq.pdf", mean_map, width = 7.5, height = 6, dpi = 300)
+ggsave("figures/mean_prevalence_map.pdf", mean_map, width = 7.5, height = 6, dpi = 300)
 
 # --------------
-results <- read.csv("data/xi_phi_meanprev_freq.csv") |>
+results <- read.csv("data/xi_phi_meanprev.csv") |>
   mutate(fadeout = ifelse(fadeout == 0, NA, fadeout))
 
 fade_prob_map <- ggplot(
@@ -69,8 +93,8 @@ fade_prob_map <- ggplot(
 ) +
   geom_tile(color = NA) +
   scale_fill_distiller(palette = "Spectral",
-                       breaks = round(seq(0,max(results$fadeout), length.out = 4),2),   # manually specify desired breaks
-                       limits = c(0, max(results$fadeout))) +
+                       breaks = round(seq(0,max(results$mean_prev), length.out = 4),1),   # manually specify desired breaks
+                       limits = c(0, 1)) +
   scale_x_continuous(breaks = round(seq(1,50,length.out = 5),0)) +
   labs(
     x = expression("super susceptible contact scaling ("*phi*")"),
@@ -91,7 +115,8 @@ fade_prob_map <- ggplot(
 
 fade_prob_map
 
-ggsave("figures/supp/strong_rare_fade_map_freq.pdf", fade_prob_map, width = 7.5, height = 6, dpi = 300)
+ggsave("figures/strong_rare_fade_map.pdf", fade_prob_map, width = 7.5, height = 6, dpi = 300)
+
 
 mean_sv_map <- ggplot(
   data = results,
@@ -103,8 +128,8 @@ mean_sv_map <- ggplot(
 ) +
   geom_tile(color = NA) +  # remove borders between tiles
   scale_fill_distiller(palette = "Spectral",
-                       breaks = round(seq(0,max(results$mean_prev), length.out = 4),4),   # manually specify desired breaks
-                       limits = c(0, max(results$mean_prev))) +
+                       breaks = round(seq(0,max(results$mean_prev), length.out = 4),1),   # manually specify desired breaks
+                       limits = c(0, 1)) +
   scale_x_continuous(breaks = round(seq(1,50,length.out = 5),0)) +
   labs(
     x = expression("super susceptible contact scaling ("*phi*")"),
@@ -124,12 +149,12 @@ mean_sv_map <- ggplot(
 
 mean_sv_map
 
-ggsave("figures/supp/strong_rare_ss_map_freq.pdf", mean_sv_map, width = 7.5, height = 6, dpi = 300)
+ggsave("figures/strong_rare_ss_map.pdf", mean_sv_map, width = 7.5, height = 6, dpi = 300)
 
 ## -----------------------------------------------------------
 ## Figure 3: fadeout. herd size x number initially exposed 
 
-results <- read.csv("data/herdsize_initnum_fadeout_freq.csv") |>
+results <- read.csv("data/herdsize_initnum_fadeout.csv") |>
   mutate(fadeout = ifelse(fadeout == 0, NA, fadeout))
 
 fade_prob_map <- ggplot(
@@ -168,13 +193,13 @@ fade_prob_map <- ggplot(
 
 fade_prob_map
 
-ggsave("figures/supp/fade_prob_map_num_freq.pdf", fade_prob_map, width = 7.5, height = 6, dpi = 300)
+ggsave("figures/fade_prob_map_num.pdf", fade_prob_map, width = 7.5, height = 6, dpi = 300)
 
 ## -----------------------------------------------------------
 ## Figure 5 (a and b): herd size x harvest intensity
 ## -----------------------------------------------------------
 
-results <- read.csv("data/hunt_herdsize_freq.csv")|>
+results <- read.csv("data/hunt_herdsize.csv") |>
   mutate(fadeout = ifelse(fadeout == 0, NA, fadeout))
 # ---------
 fade_prob_map_hunt <- ggplot(
@@ -213,7 +238,7 @@ fade_prob_map_hunt <- ggplot(
 
 fade_prob_map_hunt
 
-ggsave("figures/supp/fade_prob_map_hunt_freq.pdf", fade_prob_map_hunt, width = 7.5, height = 6, dpi = 300)
+ggsave("figures/fade_prob_map_hunt.pdf", fade_prob_map_hunt, width = 7.5, height = 6, dpi = 300)
 
 mean_map <- ggplot(
   data = results,
@@ -228,7 +253,7 @@ mean_map <- ggplot(
     palette = "Spectral",
     direction = -1,
     breaks = round(seq(0, max(results$prev_ratio, na.rm = TRUE), length.out = 5), 2),
-    limits = round(c(0, max(results$prev_ratio, na.rm = TRUE)),5),
+    limits = round(c(0, max(results$prev_ratio, na.rm = TRUE)),4),
     name = "mean estimated\nprevalence ratio"
   ) +
   scale_x_continuous(breaks = c(25, 250, 500, 750, 1000)) +
@@ -250,14 +275,16 @@ mean_map <- ggplot(
 
 mean_map
 
-ggsave("figures/supp/prev_est_map_hunt_freq.pdf", mean_map, width = 7.5, height = 6, dpi = 300)
+ggsave("figures/prev_est_map_hunt.pdf", mean_map, width = 7.5, height = 6, dpi = 300)
 
 ## -----------------------------------------------------------
-## % reduction in beta  x  % reduction in farm-contact (p2’s)
-## 
+## % reduction in p1  x  % reduction in farm-contact (p2’s)
+## Columns: hunting level (eta_hunt = 0, 0.5, 1)
+## Rows: scenarios (seeded, spillover)
+## Metrics: p(fadeout), mean infection prevalence (final tstep)
 ## -----------------------------------------------------------
 
-results <- read.csv("data/beta_p2_hunt_freq.csv") 
+results <- read.csv("data/beta_p2_hunt.csv") 
 
 p1_red_grid    <- seq(0, 1, length.out = 21)   # % reduction of p1 (0..1)
 farm_red_grid  <- seq(0, 1, length.out = 21)   # % reduction of farm-contact (0..1)
@@ -267,6 +294,7 @@ scenarios      <- c("spillover")
 results <- results %>%
   mutate(
     fadeout        = ifelse(fadeout == 0, NA_real_, fadeout),
+    # make LaTeX/plotmath style labels: eta[h]==0, eta[h]==0.5, etc.
     hunt_label = sprintf("eta[h]==%s", number(pct_hunt, accuracy = 0.01, trim = TRUE)),
     hunt_label = factor(
       hunt_label,
@@ -297,7 +325,7 @@ fade_prob_map <- ggplot(
   scale_y_continuous(expand = c(0,0), breaks = seq(0,100,25)) +
   labs(
     x = expression("% reduction in deer–deer transmission ("*beta[wild]*")"), # "% reduction in deer–deer contact (p1)",
-    y = expression("% reduction in cattle/fomite contacts ("*p[q]*")")
+    y = expression("% reduction in cattle/fomite transmission ("*beta[fq]*")")
   ) +
   facet_grid( cols = vars(hunt_label),labeller = label_parsed) +
   theme_bw(base_size = 14) +
@@ -324,15 +352,15 @@ mean_prev_map <- ggplot(
   scale_fill_distiller(
     palette = "Spectral",
     direction = -1,
-    breaks = round(seq(0, max(results$mean_prev, na.rm = TRUE), length.out = 5), 5),
-    limits = c(0, round(max(results$mean_prev, na.rm = TRUE),5)),
+    breaks = round(seq(0, max(results$mean_prev, na.rm = TRUE), length.out = 5), 2),
+    limits = c(0, round(max(results$mean_prev, na.rm = TRUE),1)),
     name   = "mean infection\nprevalence"
   ) +
   scale_x_continuous(expand = c(0,0), breaks = seq(0,100,25)) +
   scale_y_continuous(expand = c(0,0), breaks = seq(0,100,25)) +
   labs(
     x = expression("% reduction in deer–deer transmission ("*beta[wild]*")"), # "% reduction in deer–deer contact (p1)",
-    y = expression("% reduction in cattle/fomite contacts ("*p[q]*")")
+    y = expression("% reduction in cattle/fomite contacts ("*beta[fq]*")")
   ) +
   facet_grid( cols = vars(hunt_label),labeller = label_parsed) +
   theme_bw(base_size = 14) +
@@ -350,22 +378,12 @@ mean_prev_map <- ggplot(
 
 mean_prev_map
 
-ggsave("figures/supp/fade_prob_map_beta_p2_hunt_freq.pdf", fade_prob_map, width = 12, height = 5, dpi = 300)
-ggsave("figures/supp/mean_prev_map_beta_p2_hunt_freq.pdf", mean_prev_map, width = 12, height = 5, dpi = 300)
+ggsave("figures/fade_prob_map_beta_p2_hunt.pdf", fade_prob_map, width = 12, height = 5, dpi = 300)
+ggsave("figures/mean_prev_map_beta_p2_hunt.pdf", mean_prev_map, width = 12, height = 5, dpi = 300)
 
-# ---------------------------
-write.csv(R, file = paste0("./data/raw/LHS_summary_", infType, ".csv" ), row.names = F)
-remove(R)
-write.csv(bTB_wl_scaled, file = paste0("./data/raw/LHS_scaled_summary_", infType, ".csv" ), row.names = F)
-#########################################
-
-#####################
-## Monotonic Plots ##
-#####################
-# load in data if not in workspace
-# define infType variable and averaged 
-# infType = 'seeded_q1'; remove(bTB_wl_scaled)
-bTB_wl_scaled <- read.csv("data/LHS_scaled_summary_seeded_q1_freq.csv")
+##-----------------------------------------------------------
+## Sensitivity analysis
+##-----------------------------------------------------------
 
 averaged = T
 mono_plots = T
@@ -377,13 +395,13 @@ names(bTB_wl_scaled) <- c('Total Infected', 'fadeout', 'fadeout time', 'Hunt Pre
                           'start_q')
 reps = 500
 if(averaged){
-  lh <- read.csv("data/bTBwl_LHSpars_seeded_q1_freq_freq.csv")
+  lh <- read.csv("data/bTBwl_LHSpars_seeded_q1.csv")
   infType = 'seeded_q1'
   bTB_wl_agg <- aggregate.data.frame(x = bTB_wl_scaled, by = list(rep(1:dim(lh)[1], times=1, each=reps)), FUN = mean)
   bTB_wl_agg <- bTB_wl_agg[,names(bTB_wl_agg) %in% names(bTB_wl_scaled)]
   bTB_wl_scaled <- bTB_wl_agg
   infType <- paste0(infType, '_avg')
-  write.csv(bTB_wl_scaled, file = paste0("./data/LHS_scaled_summary_", infType, "_freq.csv" ), row.names = F)
+  write.csv(bTB_wl_scaled, file = paste0("./data/LHS_scaled_summary_", infType, ".csv" ), row.names = F)
 }
 
 #####################
@@ -421,7 +439,7 @@ plot_monotonicity <- function(param, infType) {
   ) |> compact()
   
   if (length(plots) > 0) {
-    outfile <- paste0("monotonic_", param, "_", infType, "_freq.jpeg")
+    outfile <- paste0("monotonic_", param, "_", infType, ".jpeg")
     jpeg(outfile, width = 840, height = 840, units = "px", res = 100)
     print(ggarrange(plotlist = plots, nrow = 2, ncol = 2))
     dev.off()
@@ -487,7 +505,7 @@ sens <- ggplot(prcc_df, aes(y = est, x = var)) +
 
 sens
 
-ggsave("figures/supp/sens_plot_prcc_freq.pdf", sens, width = 14, height = 6, dpi = 300)
+ggsave("figures/sens_plot_prcc.pdf", sens, width = 14, height = 6, dpi = 300)
 
 # --------------
 # Regression
@@ -528,7 +546,7 @@ reg <- ggplot(lm_df, aes(y = estimate, x = parameter)) +
 
 reg
 
-ggsave("figures/supp/sens_plot_reg_freq.pdf", reg, width = 14, height = 6, dpi = 300)
+ggsave("figures/supp/sens_plot_reg.pdf", reg, width = 14, height = 6, dpi = 300)
 
 
 # --------------
@@ -608,4 +626,4 @@ reg <- ggplot(lm_df, aes(y = estimate, x = parameter)) +
 
 reg
 
-ggsave("figures/supp/sens_plot_reg_int_freq.pdf", reg, width = 17, height = 10, dpi = 300)
+ggsave("figures/supp/sens_plot_reg_int.pdf", reg, width = 17, height = 10, dpi = 300)
